8ec2e07dd3505e1d81e8a902d91cbb6e7383c5fe
Feresey <p.milko1999@yandex.ru>
2018-09-11 21:22:25 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "struct.hpp"
+
+using namespace std;
+
+int main()
+{
+    string *lines = new string[MAX];
+    
+
+    return 0;
+}
---
+++ struct.hpp
@@ -0,0 +1,13 @@
+#ifndef STRUCT
+#define STRUCT
+
+const int MAX=1000,LEN=64;
+
+typedef struct
+{
+    int day;
+    int mounth;
+    int year;
+} TKey;
+
+#endif
