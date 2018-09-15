33b23538f6528266a12a031031c41d42750a06f8
Feresey <p.milko1999@yandex.ru>
2018-09-15 12:40:42 +0300

    ошибка в цикле сортировки в функции

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -2,60 +2,72 @@
 #include "struct.hpp"
 
 using namespace std;
-
-bool RadixSort(const TKey *buf1, TKey *buf2, int date, int current_bit)
+int TOTAL_LINES = 0;
+void RadixSort(const TKey *buf1, TKey *buf2, int date, int current_bit)
 {
-    bool is_max_bit_place = true; //Достигнут максимальный разряд числа
-    int write_lines = 0;
+    // bool is_max_bit_place = true; //Достигнут максимальный разряд числа
+    // int write_lines = 0;
     // int date = 0; //для переключения между днём, месяцем и годом
 
     //todo данные будут перезаписываться, если во втором буфере они уже на своём мест
 
-    is_max_bit_place = false;
+    // is_max_bit_place = false;
     for (int radix = 0; radix < 10; radix++) //пробег по цифрам
     {
-        for (int item = 0; item < MAX; item++) //для всех элеменов buf1
+        for (int item = 0; item < TOTAL_LINES; item++) //для всех элеменов buf1
         {
-            if (buf1[item].date[date] / current_bit % DECIMAL == radix)
+            if ((buf1[item].date[date] / current_bit) % DECIMAL == radix) //&& buf1[item].date[date] != buf2[item].date[date])
             {
-                if (is_max_bit_place)
-                {
-                    is_max_bit_place = true;
-                }
+                buf2[item] = buf1[item];
             }
         }
     }
-    return is_max_bit_place;
+    // return is_max_bit_place;
+}
+
+void Prnt(TKey *keys)
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        cout << keys[i].date << endl;
+    }
 }
 
 int main()
 {
-    int total_lines = 0;
     int bit = 1;
+    int current_date = 0;
+    int count = 0;
     string *lines = new string[MAX];
 
     string input;
 
     TKey *keys1 = new TKey[MAX];
     TKey *keys2 = new TKey[MAX];
-
+    TKey *buffer[2] = {keys1, keys2};
     while (getline(cin, input))
     {
         if (input != "")
         {
-            keys1[total_lines] = AddKey(input, &lines[total_lines]);
-            cout << *keys1[total_lines].line << endl;
-            total_lines++;
+            keys1[TOTAL_LINES] = AddKey(input, &lines[TOTAL_LINES]);
+            // cout << *keys1[TOTAL_LINES].line << endl;
+            TOTAL_LINES++;
         }
     }
-    for (int time = 0; time < DATE_LENGTH; time++)
+
+    //todo чот хуйня
+    for (int time : TIME_LENGTH)
     {
-        bit = 1;
-        while (RadixSort(keys1, keys2, time, bit))
+        for (int current_bit = 0; current_bit < time; current_bit++)
         {
+            RadixSort(buffer[count / 2], buffer[count / 2 + 1], current_date, bit);
             bit *= DECIMAL;
+            count++;
         }
+        current_date++;
     }
+
+    // Prnt(buffer[count / 2]);
     delete[] lines;
     delete[] keys1;
     delete[] keys2;
--- lab01/struct.hpp
+++ lab01/struct.hpp
@@ -1,5 +1,5 @@
-#ifndef STRUCT
-#define STRUCT
+#ifndef STRUCT_H
+#define STRUCT_H
 #include <iostream>
 
 using namespace std;
@@ -8,14 +8,16 @@ const int MAX = 1000;
 const int LEN = 64;
 const int DATE_LENGTH = 3; //Day, mounth and year
 const int DAY = 0, MONTH = 1, YEAR = 2;
+const int TIME_LENGTH[3] = {2, 2, 4};
 const int DECIMAL = 10;
+
 
 typedef struct
 {
     int date[DATE_LENGTH]; //Day, mounth and year
-    string *line; //указатель на строку (для экономии памяти)
+    string *line;          //указатель на строку (для экономии памяти)
 } TKey;
 
 TKey AddKey(string input, string *line);
 
-#endif
+#endif //STRUCT_H
