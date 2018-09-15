5001e7ad15464a665a03e2bc2c108713952c97cb
Feresey <p.milko1999@yandex.ru>
2018-09-15 23:09:24 +0300

    хитровыебнулся со строкой-значением, надеюсь покатит

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -6,7 +6,7 @@ int TOTAL_LINES = 0;
 void RadixSort(TKey *buf1, TKey *buf2, const int date, const int current_bit)
 {
     int write_lines = 0;
-    int digit = 0;
+    int digit = 0; //текущая цифра
 
     while (write_lines < TOTAL_LINES)
     {
@@ -21,14 +21,6 @@ void RadixSort(TKey *buf1, TKey *buf2, const int date, const int current_bit)
     }
 }
 
-void Prnt(TKey *a)
-{
-    for (int i = 0; i < TOTAL_LINES; i++)
-    {
-        std::cout << a[i].date[0] << '.' << a[i].date[1] << '.' << a[i].date[2] << '\t' << *a[i].line << std::endl;
-    }
-}
-
 int main()
 {
     int bit = 1;
@@ -36,37 +28,42 @@ int main()
     int count = 0;
 
     string *lines = new string[MAX];
+
     string input;
 
-    TKey *keys1 = new TKey[MAX];
-    TKey *keys2 = new TKey[MAX];
-    TKey *buffer[2] = {keys1, keys2};
+    TKey *input_lines = new TKey[MAX];
+    TKey *second_buffer = new TKey[MAX];
+    TKey *buffer[2] = {input_lines, second_buffer};
 
     while (getline(cin, input))
     {
         if (input != "")
         {
-            keys1[TOTAL_LINES] = AddKey(input, &lines[TOTAL_LINES]);
+            input_lines[TOTAL_LINES] = AddKey(input, &lines[TOTAL_LINES]);
             TOTAL_LINES++;
         }
     }
 
-    for (int time : TIME_LENGTH)
+    for (int time : TIME_LENGTH) //длина дня, месяца и года
     {
         bit = 1;
-        for (int current_bit = 0; current_bit < time; current_bit++)
+        for (int current_bit = 0; current_bit < time; current_bit++) //цикл для каждого десятка
         {
             RadixSort(buffer[count % 2], buffer[(count + 1) % 2], current_date, bit);
-            bit *= DECIMAL;
-            count++;
+            bit *= DECIMAL; //переход на следующую цифру
+            count++;        //для смены первого и второго буферов
         }
-        current_date++;
+        current_date++; //надеюсь, очевидно :D
     }
 
-    Prnt(buffer[count % 2]);
+    for (int i = 0; i < TOTAL_LINES; i++)
+    {
+        cout << *buffer[count % 2][i].line << endl;
+    }
 
     delete[] lines;
-    delete[] keys1;
-    delete[] keys2;
+    delete[] input_lines;
+    delete[] second_buffer;
+
     return 0;
 }
--- lab01/struct.hpp
+++ lab01/struct.hpp
@@ -8,15 +8,13 @@ const int DATE_LENGTH = 3; //Day, mounth and year
 const int DAY = 0, MONTH = 1, YEAR = 2;
 const int TIME_LENGTH[3] = {2, 2, 4};
 const int DECIMAL = 10;
-const std::string empty_string(64,'\0');
+const std::string empty_string(64, '\0');
 
 typedef struct
 {
     int date[DATE_LENGTH]; //Day, mounth and year
     std::string *line;     //указатель на строку (для экономии памяти)
 } TKey;
-
-void Prnt(TKey *a);
 
 TKey AddKey(std::string input, std::string *line);
 
