32abfed9b0f7239448ac564035551f23e4e74795
Feresey <p.milko1999@yandex.ru>
2018-09-16 17:47:17 +0300

    11 error

--- lab01/struct.hpp
+++ lab01/struct.hpp
@@ -2,7 +2,7 @@
 #define STRUCT_H
 #include <iostream>
 
-const int MAX = 1000;
+const int MAX = 100;
 const int LEN = 64;
 const int DATE_LENGTH = 3; //Day, mounth and year
 const int DAY = 0, MONTH = 1, YEAR = 2;
