1c1959429d9c4444a8e9077324e6e926a9f124b8
Feresey <p.milko1999@yandex.ru>
2018-09-16 12:00:35 +0300

    ну даже если исходные строки отправлять то ему не нравится

--- lab01/struct.cpp
+++ lab01/struct.cpp
@@ -7,27 +7,25 @@ TKey AddKey(const std::string input, std::string *line)
 {
     TKey key;
 
-    char delimiter[2] = {'.', ' '};
+    char delimiter[2] = {'.', '\t'};
     const int SIZE_OF_DELIMITER = 1;
     int prev_pos = -1;
     int pos = input.find(delimiter[0]);
-    int string_length;
-    // cout << pos << endl;
+    // int string_length;
 
     key.date[DAY] = stoi(input.substr(prev_pos + SIZE_OF_DELIMITER, pos - prev_pos)); //day
     prev_pos = pos;
     pos = input.find(delimiter[0], pos + SIZE_OF_DELIMITER);
-    // cout << pos << endl;
+
     key.date[MONTH] = stoi(input.substr(prev_pos + SIZE_OF_DELIMITER, pos - prev_pos)); //month
     prev_pos = pos;
     pos = input.find(delimiter[1], pos + SIZE_OF_DELIMITER);
-    // cout << pos << endl;
 
     key.date[YEAR] = stoi(input.substr(prev_pos + SIZE_OF_DELIMITER, pos - prev_pos)); //year
 
-    string_length = input.length() - (pos + SIZE_OF_DELIMITER);
-    cout << string_length << endl;
-    *line = input.substr(pos + SIZE_OF_DELIMITER, string_length) + empty_string.substr(0, LEN - string_length); //input line without key
+    // string_length = input.length() - (pos + SIZE_OF_DELIMITER);
+    *line = input;
+    // *line = input.substr(pos + SIZE_OF_DELIMITER, string_length) + empty_string.substr(0, LEN - string_length); //input line without key
     key.line = line;
 
     return key;
